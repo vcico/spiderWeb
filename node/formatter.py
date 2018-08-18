a9a5c4951bcd9409a6474000e1a804e630e34eed
lester <ig.join.lester@gmail.com>
2018-08-18 14:03:10 +0800

    dir

---
+++ node/formatter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+class Formatter(object):
+	"""
+	格式化数据基类
+	"""
+
+	def	_validateRequest(self):
+		pass
+	def _validateResponse(self):
+		pass
+
+
+class JsonFormatter(Formatter):
+
+	pass
+
+class XmlFormatter(Formatter):
+
+	pass
+
+
