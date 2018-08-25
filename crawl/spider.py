350122b9456d8056e181e3017b842b618ad578e7
root <root@localhost.localdomain>
2018-08-25 20:44:01 +0800

    x

---
+++ crawl/spider.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import requests
+
+class Spider
+{
+	def __init__(self):
+		pass
+
+}
--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+
+
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
+
 @app.route("/<path:url>")
 def mirror(url):
 	return "the url is %s" % url
